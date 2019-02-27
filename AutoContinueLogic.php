50eb84d3cda00373a311149d2219404267c0e9f2
Andrew Martin <andy123@stanford.edu>
2019-02-27 14:45:40 -0800

    fixed bug with adaptive instruments

--- AutoContinueLogic.php
+++ AutoContinueLogic.php
@@ -8,14 +8,25 @@
 
 namespace Stanford\AutoContinueLogic;
 
+include_once("emLoggerTrait.php");
+
+use \REDCap;
+use \Survey;
+
 class AutoContinueLogic extends \ExternalModules\AbstractExternalModule {
 
+    use emLoggerTrait;
 
-    function  hook_survey_page_top($project_id, $record = NULL, $instrument, $event_id, $group_id = NULL, $survey_hash = NULL, $response_id = NULL, $repeat_instance = 1)
-    {
 
-        $auto_continue_logic = array();
+    /**
+     * LOAD THE CONFIG - FIRST USING THE JSON FORMAT AND THEN BASED ON EACH FIELD AS SPECIFIED
+     * @param $project_id
+     * @return array|mixed
+     */
+    function loadConfig($project_id) {
+
         $all_settings = $this->getProjectSettings($project_id);
+        $auto_continue_logic = array();
 
         // First load the json options (stripping linefeeds)
         if (! empty($all_settings['autocontinue_logic']['value'])) {
@@ -32,6 +43,23 @@ class AutoContinueLogic extends \ExternalModules\AbstractExternalModule {
             }
         }
 
+        return $auto_continue_logic;
+    }
+
+    function redcap_survey_page_top($project_id, $record = NULL, $instrument, $event_id, $group_id = NULL, $survey_hash = NULL, $response_id = NULL, $repeat_instance = 1)
+    {
+
+        // $this->emDebug("At SurveyPage Top of ACL", func_get_args(), $_POST);
+
+        // Don't do anything if there isn't a record_id - and for promis-cat instruments
+        if (empty($record)) {
+            $this->emDebug('Record missing - returning');
+            return;
+        }
+
+        $auto_continue_logic = $this->loadConfig($project_id);
+
+
         // Check if custom logic is applied to this instrument
         if (isset ($auto_continue_logic [$instrument])) {
             // Get the logic and evaluate it
@@ -39,70 +67,48 @@ class AutoContinueLogic extends \ExternalModules\AbstractExternalModule {
 
             $this->emDebug($auto_continue_logic, "Instrument: " . $instrument, "Raw Logic", $raw_logic);
 
-            // \Plugin::log($raw_logic, "DEBUG", "$project_id: Logic Before: " . $raw_logic);
-            //// Prepend current event as needed
-            //if (\REDCap::isLongitudinal()) {
-            //    $unique_event_name = \REDCap::getEventNames(true,false,$event_id);
-            //    // \Plugin::log("$project_id: unique_event_name: " . $unique_event_name);
-            //    $raw_logic = \LogicTester::logicPrependEventName($raw_logic, $unique_event_name);
-            //    // \Plugin::log("$project_id: logic after: " . $raw_logic);
-            //}
-            //
-            //$isValid = \LogicTester::isValid($raw_logic);
-            //
-            //if (!$isValid) {
-            //    print "<div class='red'><h3><center>Supplied survey auto-continue logic is invalid:<br>$raw_logic</center></h3></div>";
-            //}
-            //$logic_result = \LogicTester::evaluateLogicSingleRecord($raw_logic, $record);
+            $logic_result = REDCap::evaluateLogic($raw_logic, $project_id, $record, $event_id, $repeat_instance); //, $instrument);
+            // $this->emDebug("EVALUATING: ", $raw_logic, $project_id, $record, $event_id, $repeat_instance, "RESULT:", $logic_result);
 
-            $logic_result = \REDCap::evaluateLogic($raw_logic, $project_id, $record, $event_id, $repeat_instance); //, $instrument);
-            // \Plugin::log("- $record at $instrument with [" . ($logic_result ? "true" : "false") . "] from $raw_logic");
+            $this->emDebug("Logic Result",
+                    $logic_result,
+                    "Raw: " . $raw_logic,
+                    "PID: " . $project_id,
+                    "RECORD: " . $record,
+                    "EVENT ID: " . $event_id,
+                    "INSTANCE: " . $repeat_instance,
+                    "INSTRUMENT: " . $instrument);
 
             if ($logic_result == false) {
-                $this->emDebug("Logic Result FALSE", "Raw: " . $raw_logic, $logic_result, "PID: " . $project_id, "RECORD: " . $record, "EVENT ID: " . $event_id, "INSTANCE: " . $repeat_instance, "INSTRUMENT" . $instrument);
-
                 // If autocontinue is enabled - then redirect to next instrument
-                \REDCap::logEvent("$instrument skipped due to AutoContinue Logic EM", "", "", $record, $event_id, $project_id);
-                // $this->exitAfterHook();
+                REDCap::logEvent("$instrument skipped due to AutoContinue Logic EM", "", "", $record, $event_id, $project_id);
 
                 global $end_survey_redirect_next_survey;
-
                 if ($end_survey_redirect_next_survey) {
                     // Try to get the next survey url
-                    $next_survey_url = \Survey::getAutoContinueSurveyUrl($record, $instrument, $event_id, $repeat_instance);
-                    // print "Redirecting you to $next_survey_url";
-                    // \Plugin::log("Soft Redirecting $record from $instrument to $next_survey_url");
-                    // This is causing issues so I'm going to try a client-redirect
-                    // redirect($next_survey_url);
+                    $next_survey_url = Survey::getAutoContinueSurveyUrl($record, $instrument, $event_id, $repeat_instance);
+
+                    $this->emDebug("Redirecting client to next survey: " . $next_survey_url);
+
                     $this->redirect($next_survey_url);
                     return false;
                 } else {
                     // If there is a normal end-of-survey url - go there
                     global $end_survey_redirect_url;
                     if ($end_survey_redirect_url != "") {
-                        // \Plugin::log("Redirecting to end_survey_redirect_url $record from $instrument to $end_survey_redirect_url");
-                        // redirect($end_survey_redirect_url);
+                        $this->emDebug("Redirecting to end_survey_redirect_url: " . $next_survey_url, "Record $record / Instrument $instrument");
                         $this->redirect($end_survey_redirect_url);
                         return false;
-                    }                // Display the normal end-of-survey message with an additional note
-                    else {
+                    } else {
                         // Get full acknowledgement text (perform piping, if applicable)
+                        $this->emDebug("Displaying acknowledgement");
                         global $acknowledgement;
-                        $full_acknowledgement_text = "Thanks for taking the survey.";
-
-                        $custom_text = ""; // "<div class='yellow'><h3><center>This survey does not apply.</center></h3></div>";
-                        // \Plugin::log("Survey logic is false for $record from $instrument but no auto-continue is enabled - displaying end of survey text.");
-                        // \Plugin::log("Calling ExitSurvey from $instrument to $full_acknowledgement_text / $acknowledgement");
-
-                        // The acknowledgement text comes from the last survey
                         exitSurvey($acknowledgement, false);
                     }
                 }
-                // Catch all
-                // exit ();
             } else {
-                $this->emDebug("Logic Result TRUE", "Raw: " . $raw_logic, $logic_result, "PID: " . $project_id, "RECORD: " . $record, "EVENT ID: " . $event_id, "INSTANCE: " . $repeat_instance, "INSTRUMENT" . $instrument);
                 // administer the instrument
+                $this->emDebug("Logic valid - administering $instrument for record $record");
             }
         }
     }
@@ -113,30 +119,5 @@ class AutoContinueLogic extends \ExternalModules\AbstractExternalModule {
         // Doing a soft-redirect so that we can return from the hook and not throw and EM error
         echo("<script type=\"text/javascript\">window.location.href=\"$url\";</script>");
     }
-
-
-    /**
-     *
-     * emLogging integration
-     *
-     */
-    function emLog() {
-        $emLogger = \ExternalModules\ExternalModules::getModuleInstance('em_logger');
-        $emLogger->emLog($this->PREFIX, func_get_args(), "INFO");
-    }
-
-    function emDebug() {
-        // Check if debug enabled
-        if ($this->getSystemSetting('enable-system-debug-logging') || (!empty($_GET['pid']) && $this->getProjectSetting('enable-project-debug-logging'))) {
-            $emLogger = \ExternalModules\ExternalModules::getModuleInstance('em_logger');
-            $emLogger->emLog($this->PREFIX, func_get_args(), "DEBUG");
-        }
-    }
-
-    function emError() {
-        $emLogger = \ExternalModules\ExternalModules::getModuleInstance('em_logger');
-        $emLogger->emLog($this->PREFIX, func_get_args(), "ERROR");
-    }
-
 
 }
